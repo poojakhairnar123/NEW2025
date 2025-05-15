887a11ddce8c23500027d2cf7240852973b7236d
poojakhairnar <pkhairnar990@gmail.com>
2025-05-15 17:08:27 +0530

    add

--- com.amazon.in/src/test/java/com/amazon_in_Test/Home_page_loads_successfully_Test.java
+++ com.amazon.in/src/test/java/com/amazon_in_Test/Home_page_loads_successfully_Test.java
@@ -3,6 +3,14 @@ package com.amazon_in_Test;
 public class Home_page_loads_successfully_Test{
 
 	 // FileInputStream files = new FileInputStream("");
-     System.out.println("DONE");
+   //  System.out.println("DONE");
 
+	 // FileInputStream files = new FileInputStream("");
+  //  System.out.println("DONE");
+
+	 // FileInputStream files = new FileInputStream("");
+  //  System.out.println("DONE");
+
+	 // FileInputStream files = new FileInputStream("");
+  //  System.out.println("DONE");
 }
